66ac352885aada9bc00ca218d3073f71e85d0446
João Lucas <jlso9866@gmail.com>
2026-05-29 14:03:59 -0300

    feat(model): created aditable abstract entity

---
+++ identity_service/internal/domain/model/auditable.go
@@ -0,0 +1,49 @@
+package model
+
+import "time"
+
+//Um objeto auditavel presenta as informações de criação e manipulação das entidades
+// 
+//Estrutura basilar do sistema: serve de base para quase todas as entidades do sistema
+type Auditable struct {
+	createdAt *time.Time
+	updatedAt *time.Time
+	deletedAt *time.Time //soft delete
+}
+
+func (a *Auditable) getCreatedAt() *time.Time{
+	return a.createdAt
+}
+
+func (a *Auditable) getUpdatedAt() *time.Time{
+	return a.updatedAt
+}
+
+func (a *Auditable) getDeletedAt() *time.Time{
+	return a.deletedAt
+}
+
+func (a *Auditable) setUpdatedAt(updatedAt *time.Time){
+	a.updatedAt = updatedAt
+}
+
+func (a *Auditable) setDeletedAt(deletedAt *time.Time){
+	a.deletedAt = deletedAt
+}
+
+func NewAuditable() (*Auditable, error) {
+	//timezone america/saoPaulo
+	location, err := time.LoadLocation("America/Sao_Paulo")
+
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now().In(location)
+	
+	return &Auditable{
+		createdAt: &now,
+		updatedAt: nil,
+		deletedAt: nil,
+	}, nil
+}
